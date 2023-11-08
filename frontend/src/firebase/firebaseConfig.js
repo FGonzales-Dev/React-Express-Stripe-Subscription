c03c74c88d9d5224fb9911862fabc79a86c84adb
francis gonzales <fgonzales.private@icloud.com>
2023-11-08 21:12:51 +0800

    initial commit

---
+++ frontend/src/firebase/firebaseConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/database"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC3F-w3F_z4vUbS7Bth9YZLbo_EoG8--PM",
+    authDomain: "stripe-subscription-prototype.firebaseapp.com",
+    databaseURL: "https://stripe-subscription-prototype-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "stripe-subscription-prototype",
+    storageBucket: "stripe-subscription-prototype.appspot.com",
+    messagingSenderId: "1088853797108",
+    appId: "1:1088853797108:web:3b3e8e660a026cf05f96a1",
+    measurementId: "G-D2LPNDSLN6"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
