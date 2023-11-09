5e19046824a868d2e4b3b6fa6f5c32178810a9b9
francis gonzales <fgonzales.private@icloud.com>
2023-11-09 09:58:35 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ admin.initializeApp({
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://react-express-stripe-subscription-djgf.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://react-express-stripe-subscription-djgf.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
